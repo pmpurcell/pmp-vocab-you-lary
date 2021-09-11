57f9b9d7677bd5bcbaf8381bbd449980c595442b
pmpurcell93 <maddenpurcell@gmail.com>
2021-09-11 14:13:43 -0500

    Added search bar function

--- src/scripts/components/navBuilder.js
+++ src/scripts/components/navBuilder.js
@@ -13,6 +13,12 @@ const navBuilder = () => {
         <a class="nav-link active" aria-current="page" id="home">Home</a>
         <a class="nav-link" id="new-word">New Word!</a>
       </div>
+      <div id="search-div">
+      <form class="d-flex" id="search">
+      <input class="form-control me-2" id="search-value" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+      </div>
       <div id="logout"></div>
     </div>
   </div>
